e4c2f9e1628e3fa88aaa473e1b82f78929034ca5
olskalim <is.2003.01@gmail.com>
2021-03-18 23:19:15 +0300

    added SD support and RTC synchronization

--- src/sensors.cpp
+++ src/sensors.cpp
@@ -3,6 +3,9 @@
 #include <Adafruit_Sensor.h>  // библиотека для работы с MQ-135
 #include <SoftwareSerial.h>   // библиотека для передачи данных в esp
 #include <MQ135.h>            // библиотека для работы с MQ-135
+#include <TimeLib.h>          // библиотека для пересчета реального времени
+#include <SPI.h>
+#include <SD.h>
 
 /*
 Датчики: 
@@ -19,8 +22,9 @@
 #define MOTION_PIN 2 // пин датчика движения
 #define NOISE_PIN A1 // пин датчика шума
 #define CO2_PIN A2   // пин датчика газов
-SoftwareSerial espSerial(10, 11); // инициализация второго Serial порта для esp (recieve, transmit)
-DHT dht(DHT_PIN, DHT11);          // инициализация датчика температуры
+#define SD_PIN 4     // пин sd карты
+SoftwareSerial espSerial(8, 9); // инициализация второго Serial порта для esp (RX, TX)
+DHT dht(DHT_PIN, DHT11);        // инициализация датчика температуры
 MQ135 mq(CO2_PIN);
 
 float rawData0[6]; // три массива с сырыми данными, обновляющимися по очереди
@@ -28,55 +32,76 @@ float rawData1[6];
 float rawData2[6];
 float data[6];     // массив с отфильтрованными данными
 byte n = 0;        // счетчик для чтения в массив с сырыми данными
-unsigned long last_read = 0;
+unsigned long last_read = 0; // переменная для таймера чтения
+time_t real_time = 0;        // переменная RTC
 
 float readData(int sensor);     // функция универсального чтения для всех датчиков
 void sendToSerial(float data[]);// передача данных на компьютер
 void sendToEsp(float data[]);   // передача данных на esp
 void medianFilter(float *refRawData0, float *refRawData1, float *refRawData2, float* refData); // медианный фильтр для защиты от некорректных данных
 void readToRaw(float *refRawData0, float *refRawData1, float *refRawData2, byte &iter); // чтение в массив с сырыми данными
+void timeUpdate();            // обновление RTC
+void sendData();              // передача аднных в esp
+void writeToSD(float data[]); // запись данных на SD карту
 
 
 void setup() {
     Serial.begin(9600);         // установка связи с компьютером
-    espSerial.begin(9600);
+    espSerial.begin(9600);      // установка связи с esp
     espSerial.setTimeout(50);   // таймаут для получения данных от esp
     pinMode(LIGHT_PIN, INPUT);  // инициализация фоторезистора
     pinMode(NOISE_PIN, INPUT);  // инициализация микрофона
     pinMode(CO2_PIN, INPUT);    // инициализация датчика CO2
     dht.begin();                // инициализация датчика температуры/влажности
-    pinMode(13, OUTPUT);        // работа со внутренним светодиодом
-    digitalWrite(13, LOW);
+    pinMode(10, OUTPUT);        // инициализация SD карты
+    SD.begin(SD_PIN);
     for (int i = 0; i < 3; i++) {
         readToRaw(rawData0, rawData1, rawData2, n); // трижды читаем данные с датчиков, чтобы "прогреть" их
                                                     // и для корректной работы медианного фильтра
         n++;
         delay(1000);
     }
-    for (int i = 0; i < 5; i++) { // сигнализируем об окончании инициализации
-        digitalWrite(13, HIGH);
-        delay(75);
-        digitalWrite(13, LOW);
-        delay(75);
-    }
 }
 
 
 void loop() {
-    if (last_read - millis() > 1000) {
-        last_read = millis();
+    if (millis() - last_read > 1000) {
+        last_read = millis(); // раз в секунду читаем информацию с датчиков
         readToRaw(rawData0, rawData1, rawData2, n);
     }
-    if (espSerial.available()) {
+    if (espSerial.available()) { // постоянно следим за esp и обрабаотываем команды от неё
         String message = espSerial.readString();
-        Serial.print("Recieved from ESP: ");
+        Serial.print("\nRecieved from ESP: ");
         Serial.println(message);
         if (message == "$get") {
-            medianFilter(rawData0, rawData1, rawData2, data); // прогоняем прочитанные данные через фильтр
-            sendToEsp(data); // отправляем их в esp
-            sendToSerial(data);  // и на ПК
+            sendData(); // передаем отфильтрованные данные
+        }
+        else if (message == "$time") {
+            timeUpdate(); // обновляем RTC 
         }
     }
+}
+
+
+void timeUpdate() { // обновляем RTC 
+    espSerial.print("$ready");
+    char t[20];
+    while (!espSerial.available()) {}
+    espSerial.readString().toCharArray(t, 20);
+    real_time = time_t(strtoul(t, NULL, 10));
+    setTime(real_time);
+    Serial.print("Time has been sucsessfully updated! Now it is: ");
+    Serial.println(real_time);
+    Serial.println();
+}
+
+
+void sendData() {
+    medianFilter(rawData0, rawData1, rawData2, data); // прогоняем прочитанные данные через фильтр
+    sendToEsp(data); // отправляем их в esp
+    sendToSerial(data); // на ПК
+    writeToSD(data); // и записываем на карту памяти
+    Serial.println();
 }
 
 
@@ -88,7 +113,7 @@ float readData(int sensor) {
         return dht.readHumidity(); // чтение с DHT
     }
     if (sensor == 2) {
-        return mq.getPPM(); // чтение с MQ-135
+        return mq.getPPM()/1000; // чтение с MQ-135
     }
     if (sensor == 3) {
         if (digitalRead(MOTION_PIN)) return 100; // чтение с датчика движения
@@ -108,8 +133,28 @@ float readData(int sensor) {
 }
 
 
+void writeToSD(float data[]) {
+    String info = "";
+    info += String(now()); // добавляем к строке для записи текущее время
+    for (int i = 0; i < 6; i++) {
+        info += ",";
+        info += String(data[i]); // и информацию со всех датчиков
+    }
+    Serial.print("Writing to sd: ");
+    Serial.println(info);
+    File dataFile = SD.open("datalog.csv", FILE_WRITE);
+    if (dataFile) {
+        dataFile.println(info);
+        dataFile.close();
+        Serial.println("Sucsessfully written data!");
+    }
+    else {
+        Serial.println("Error opening file.");
+    }
+}
+
+
 void sendToSerial(float data[]) {
-    digitalWrite(13, HIGH);
     Serial.print("Temperature: ");     Serial.print(data[0]); Serial.println("°C");
     Serial.print("Humidity: ");        Serial.print(data[1]); Serial.println("%");
     Serial.print("CO2 level: ");       Serial.print(data[2]); Serial.println("%");
@@ -117,8 +162,8 @@ void sendToSerial(float data[]) {
     else {Serial.println("No motion detected.");}
     Serial.print("Light intensity: "); Serial.print(data[4]); Serial.println("%");
     Serial.print("Noise level: ");     Serial.print(data[5]); Serial.println("%");
+    Serial.print("Current time: "); Serial.print(hour()); Serial.print(":"); Serial.print(minute()); Serial.print(":"); Serial.print(second()); 
     Serial.println();
-    digitalWrite(13, LOW);
 }
 
 
@@ -143,6 +188,7 @@ void medianFilter(float *refRawData0, float *refRawData1, float *refRawData2, fl
         refData[k] = m;
     }
 }
+
 
 void readToRaw(float *refRawData0, float *refRawData1, float *refRawData2, byte &iter) {
     for (int k = 0; k < 6; k++) { // к сожалению, на данном этапе не удалось реализовать 
